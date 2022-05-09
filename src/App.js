30a43ef198ffdca48a45a3bd1886eb8098ada17b
unknown <nurmuhammadnarynbekuulu@gmail.com>
2022-05-09 17:59:42 +0600

    my first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header/>
+        <Main/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
